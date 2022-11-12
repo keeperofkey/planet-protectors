0c08646231f561146a717773e6db90e1ada6811e
keeperofkey <doddtastic@gmail.com>
2022-11-12 11:12:28 -0700

    modified:   image-test.py

--- image-test.py
+++ image-test.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_APIKEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.Image.create(
   prompt="A cute baby sea otter",
   n=2,
