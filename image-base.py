60ddbea4f0b61b9a6512ed759b4189888fdd8214
keeperofkey <doddtastic@gmail.com>
2022-11-12 13:14:54 -0700

    new file:   image-base.py 	deleted:    image-test.py

---
+++ image-base.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+import os
+import openai
+import requests
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+concept = openai.Completion.create(
+  model="text-davinci-002",
+  prompt="Here is a concept about climate change:\n",
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+print(concept.choices[0].text)
+scene = openai.Completion.create(
+  model="text-davinci-002",
+  prompt = concept.choices[0].text + "Describe a funny image that represents this:\n",
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+print(scene.choices[0].text)
+caption = openai.Completion.create(
+  model="text-davinci-002",
+  prompt = concept.choices[0].text + scene.choices[0].text + "With a funny caption:\n",
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+) 
+print(caption.choices[0].text)
+image = openai.Image.create(prompt=scene.choices[0].text, n=1, size="1024x1024")
+print(image.data[0].url)
+
+img_data = requests.get(image.data[0].url).content
+with open('{}.jpg'.format(caption.choices[0].text), 'wb') as handler:
+    handler.write(img_data)
+
+
+
--- image-test.py
+++
@@ -1,9 +0,0 @@
-import os
-import openai
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Image.create(
-  prompt="A cute baby sea otter",
-  n=2,
-  size="1024x1024"
-)
