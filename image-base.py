441e391c54505e8d59151f6149182e30f21f00f4
keeperofkey <doddtastic@gmail.com>
2022-11-12 19:24:12 -0700

    stevens prompt revisions + draw caption on image

--- image-base.py
+++ image-base.py
@@ -2,13 +2,13 @@
 import os
 import openai
 import requests
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 concept = openai.Completion.create(
   model="text-davinci-002",
-  prompt="climate change is ",
+  prompt="Generate a concept related to climate change:\n",
   temperature=1,
   max_tokens=64,
   top_p=1,
@@ -18,8 +18,8 @@ concept = openai.Completion.create(
 print(concept.choices[0].text)
 scene = openai.Completion.create(
   model="text-davinci-002",
-  prompt = concept.choices[0].text + "Describe an image that represents this:\n",
-  temperature=0.7,
+  prompt="Describe a funny image representing this:\n" + concept.choices[0].text,
+  temperature=1,
   max_tokens=128,
   top_p=1,
   frequency_penalty=1,
@@ -28,9 +28,9 @@ scene = openai.Completion.create(
 print(scene.choices[0].text)
 caption = openai.Completion.create(
   model="text-davinci-002",
-  prompt = concept.choices[0].text + scene.choices[0].text + "With a funny caption:\n",
-  temperature=0.7,
-  max_tokens=256,
+  prompt="Write a funny caption about this:\n" + scene.choices[0].text,
+  temperature=1,
+  max_tokens=64,
   top_p=1,
   frequency_penalty=1,
   presence_penalty=1
@@ -40,12 +40,10 @@ image = openai.Image.create(prompt=scene.choices[0].text, n=1, size="1024x1024")
 print(image.data[0].url)
 
 img_data = requests.get(image.data[0].url).content
-print(img_data)
-
-#draw = ImageDraw.Draw(img_data)
-#draw.text((28, 36), "{}".format(caption.choices[0].text), fill=(255, 0, 0))
 with open("./images/{}.jpg".format(caption.choices[0].text), 'wb') as handler:
     handler.write(img_data)
-
-
-
+img = Image.open("./images/{}.jpg".format(caption.choices[0].text))
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype('SourceCodePro-Black.otf', 24)
+draw.multiline_text((0, 0), "{}".format(caption.choices[0].text), fill=(255, 0, 0), font=font)
+img.save("./images/{}.jpg".format(caption.choices[0].text))
