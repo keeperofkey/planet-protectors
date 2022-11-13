fc12ed7b3cb635af4e9ec81bc503bf4139fb8b77
Steven Finkelstein <sfinkelstein20@gmail.com>
2022-11-13 12:25:53 -0500

    Updated gpt3 calls slightly; added metadata export

--- openai_testing_sf.py
+++ openai_testing_sf.py
@@ -9,6 +9,9 @@ import numpy as np
 import openai
 import os
 import json
+from PIL import Image, ImageFont, ImageDraw
+import re
+import requests
 
 #source: https://www.youtube.com/watch?v=VTx0xBPOv5Q
 #Source2: https://www.youtube.com/watch?v=9971sxBhEyQ
@@ -92,12 +95,34 @@ fine_tuned_model = 'davinci:ft-oai-hackathon-2022-team-41-2022-11-12-22-01-38'
 
 'text-davinci-002'
 
-
+'''
+#################################################################################
+##############################          Start here          ###################################################
+############################################################################################
+'''
 
 
 '''Liam's code with slight tweaks to parameters and prompts'''
 
 '''Creating prompts for the climate change topic, image, and caption'''
+
+
+'''Create lists for df later on'''
+i = 0
+climate_concepts = []
+image_descriptions = []
+image_captions = []
+image_names = []
+image_links = []
+
+
+'''Start here'''
+#climate_concepts.pop()
+#image_descriptions.pop()
+#image_captions.pop()
+
+#print(len(climate_concepts))
+#print(len(image_names))
 
 concept = openai.Completion.create(
     model='text-davinci-002',
@@ -110,13 +135,14 @@ concept = openai.Completion.create(
     )
 
 print(concept.choices[0].text)
+climate_concepts.append(concept.choices[0].text.strip())
 
 #"Describe a funny image that represents this:\n",
 #concept.choices[0].text + scene.choices[0].text + "With a funny caption:\n",
 
 scene = openai.Completion.create(
     model='text-davinci-002',
-    prompt = "Describe a funny image representing this:\n" + concept.choices[0].text,
+    prompt = "Describe a funny image representing this:\n" + concept.choices[0].text.strip(),
     temperature=1,
     max_tokens=30,
     top_p=1,
@@ -125,17 +151,19 @@ scene = openai.Completion.create(
 )
 
 print(scene.choices[0].text)
+image_descriptions.append(scene.choices[0].text.strip())
 
 caption = openai.Completion.create(
     model='text-davinci-002',
-    prompt = "Write a funny caption about this:\n" + scene.choices[0].text,
-    temperature=0.7,
+    prompt = "Generate a funny caption about this:\n" + scene.choices[0].text.strip(),
+    temperature=1,
     max_tokens=30,
     top_p=1,
     frequency_penalty=1,
     presence_penalty=0
     ) 
 print(caption.choices[0].text)
+image_captions.append(caption.choices[0].text.strip())
 
 '''
 test = 'funny Climate change refers to a broad array of environmental degradation that is predicted to'
@@ -144,33 +172,67 @@ image = openai.Image.create(prompt=test, n=1, size="1024x1024")
 print(image.data[0].url)
 '''
 
-image = openai.Image.create(prompt=scene.choices[0].text, n=1, size="1024x1024")
+image = openai.Image.create(prompt=scene.choices[0].text.strip(), n=1, size="1024x1024")
 print(image.data[0].url)
+image_links.append(image.data[0].url)
+
+image_name = caption.choices[0].text[0:10]
+
+'''Remove special characters from image file name'''
+image_name =re.sub("[^A-Z]", "", image_name,0,re.IGNORECASE)
+image_name = image_name.strip()
+image_name = image_name + '_' + str(i)
+image_names.append(image_name)
+print(image_name)
+i+=1
+
+
+'''Save picture manually first'''
+response = requests.get(image.data[0].url)
+file_name = file_import_location + 'images_generated/' + image_name + '.jpg'
+if response.status_code == 200:
+    with open(file_name, "wb") as f:
+        f.write(response.content)
+        
+        
+'''Open image and add caption'''
+# Open an Image
+temp_img = Image.open(file_name)
+ 
+# Call draw Method to add 2D graphics in an image
+I1 = ImageDraw.Draw(temp_img)
+ 
+# Add Text to an image
+fontsize = 30
+font = ImageFont.truetype("arial.ttf", fontsize)
+I1.text((28, 36), caption.choices[0].text , font= font, fill=(255, 0, 0))
+ 
+# Display edited image
+temp_img.show()
+ 
+# Save the edited image
+file_name_withcaption = file_import_location + 'images_generated/' + image_name + '_withcaption.jpg'
+temp_img.save(file_name_withcaption)
 
 
 
-img_data = requests.get(image.data[0].url).content
-with open("./images/{}.jpg".format(caption.choices[0].text), 'wb') as handler:
-    handler.write(img_data)
+'''Create a dataframe by zipping together all of the lists'''
+
+list_of_tuples = list(zip(climate_concepts, image_descriptions, image_captions, image_names,image_links ))
+climate_change_images_df = pd.DataFrame(list_of_tuples, columns = ['Climate_Concept', 'Image_Description', 'Image_Caption', 'Image_Filename', 'Image_Link'])
+climate_change_images_df.head()
 
 
 
 
+'''Export to Excel'''
 
-'''
+#Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter(file_import_location + 'climate_change_gpt3_image_data.xlsx', engine='xlsxwriter')
 
-Next steps
-
-automate putting caption in image
-
-prompt engineering??? Clean up text being fed into the models.
-
-
-take first split of first response....split by period/exclamation point
-'''
+# Write each dataframe to a different worksheet.
+climate_change_images_df.to_excel(writer, sheet_name='Image_Data', index=False)
 
 
-
-
-
-
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
